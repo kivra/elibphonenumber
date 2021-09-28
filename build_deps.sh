18eb7a86594bded45e884639ae0cb01e2352ddb9
Enrique Fernandez <enrique.fernandez@kivra.com>
2021-09-28 20:25:18 +0200

    Use libphonenumber-dev

--- build_deps.sh
+++ build_deps.sh
@@ -126,7 +126,7 @@ install_libphonenumber()
                     ;;
                 *)
                     # Assume Alpine Linux
-                    apk --no-cache add libphonenumber
+                    apk --no-cache add libphonenumber-dev
             esac
         ;;
 
